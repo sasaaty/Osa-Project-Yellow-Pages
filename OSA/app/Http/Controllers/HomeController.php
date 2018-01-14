366acaa9807da05601c054ada4438b0a50f295e1
Adrian Danao <adrian_danao01@yahoo.com>
2018-01-14 15:56:28 +0800

    search function

--- OSA/app/Http/Controllers/HomeController.php
+++ OSA/app/Http/Controllers/HomeController.php
@@ -14,6 +14,7 @@ class HomeController extends Controller
     	$suppliers = Supplier::when($category, function ($query) use($category){
     							return $query->where('category_id', $category);
     						})
+                            ->where('company_name', 'LIKE', '%'.$search.'%')
                             ->where('state', "Accepted")
     						->orderBy('rating', 'desc')
     						->paginate(12);
